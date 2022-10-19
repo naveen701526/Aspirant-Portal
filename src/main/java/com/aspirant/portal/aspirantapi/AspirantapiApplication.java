8023b3211a46b7b856afc630b38e32fb0d22777e
“JeevanSainik789” <mettakumari72@gmail.com>
2022-10-19 23:37:10 +0530

    start point project backend

---
+++ src/main/java/com/aspirant/portal/aspirantapi/AspirantapiApplication.java
@@ -0,0 +1,13 @@
+package com.aspirant.portal.aspirantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspirantapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspirantapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aspirant/portal/aspirantapi/AspirantapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspirant.portal.aspirantapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspirantapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
